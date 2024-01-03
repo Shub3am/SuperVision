c4847b94b6dcfba59e008fee6c0495965209137d
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-01-03 21:45:56 +0530

    Fixed Bug in Layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark:bg-gray-900`}><Header/>{children}</body>
+      <body className={`${inter.className} bg-gray-900`}><Header/>{children}</body>
     </html>
   )
 }
